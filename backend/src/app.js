76d1991be4ab48057d84bac909ac6037df1a667d
neennera <neennera@gmail.com>
2024-04-22 07:46:10 +0700

    refacter: refacter some code

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import UserRoute from "./routes/userRoute.js"
 import RoomRoute from "./routes/roomRoute.js"
 import CPmonRoute from "./routes/CPmonRoute.js"
-import { FRONTEND_URL, BACKEND_URL, GOTO_URL } from "../../frontend/public/scripts/config.js";
+import { FRONTEND_URL, BACKEND_URL } from "../../frontend/public/scripts/config.js";
 const app = express();
 
 // body-parser
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // allow request from other origin (Frontend which is at different port)
 app.use(cors({
-    origin: [FRONTEND_URL, BACKEND_URL, GOTO_URL],
+    origin: [FRONTEND_URL, BACKEND_URL],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
