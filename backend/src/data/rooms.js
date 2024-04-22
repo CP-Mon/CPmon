8c354d1453223e0f073a9fb3808c9200b601a2b3
Kritsakorn Sataratpayoon <zxcnadeems@gmail.com>
2024-04-22 22:34:25 +0700

    rename: rooms

--- backend/src/data/rooms.js
+++ backend/src/data/rooms.js
@@ -2,8 +2,12 @@ import Room from '../models/Room.js';
 
 const rooms = [];
 
-for(let i = 0; i < 6; i++) {
-    rooms.push(new Room(`CPmon-${i+1}`));
-}
+rooms.push(new Room(`LarnIntania`));
+rooms.push(new Room(`iCanteen`));
+rooms.push(new Room(`Parabola`));
+rooms.push(new Room(`SkyCafe`));
+rooms.push(new Room(`ComputerCenter`));
+rooms.push(new Room(`Engineering4`));
+
 
 export default rooms;
