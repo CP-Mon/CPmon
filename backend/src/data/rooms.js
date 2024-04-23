d7f6216b88679469873ef7b313ae46d827023efa
Brnn043 <95439638+Brnn043@users.noreply.github.com>
2024-04-23 19:03:02 +0700

    added more rooms

--- backend/src/data/rooms.js
+++ backend/src/data/rooms.js
@@ -8,6 +8,11 @@ rooms.push(new Room(`Parabola`));
 rooms.push(new Room(`SkyCafe`));
 rooms.push(new Room(`ComputerCenter`));
 rooms.push(new Room(`Engineering4`));
-
+rooms.push(new Room(`Library`));
+rooms.push(new Room(`100YearBuilding`));
+rooms.push(new Room(`TheBox`));
+rooms.push(new Room(`Amazon`));
+rooms.push(new Room(`TaoBin`));
+rooms.push(new Room(`HallOfIntania`));
 
 export default rooms;
